603280da78b24d18916732abf1d519723fceb123
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-08 17:34:36 +0200

    changes

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-print('Sabor Express\n');
+print('𝒮𝒶𝒷ℴ𝓇 ℰ𝓍𝓅𝓇ℯ𝓈𝓈\n');
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair \n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 
-print('\nVoce escolheu a opção {}'.format(opcao_escolhida))
+
+if opcao_escolhida == 1:
+    print('Cadastrando restaurante...')
+elif opcao_escolhida == 2:
+    print('Listando restaurante...')    
+elif opcao_escolhida == 3:
+    print('Ativando restaurante...') 
+else:
+    print('Saindo...')       
