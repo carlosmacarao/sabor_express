458d06b0d05de57518df47e7896698fe32908763
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-08 16:40:04 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+print('Sabor Express\n');
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print('\nVoce escolheu a opção {}'.format(opcao_escolhida))
