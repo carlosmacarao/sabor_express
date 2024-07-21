409afc9949acd7a815fc545c137384c484449c05
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-21 23:58:21 +0200

    new change

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def alterar_estado_restaurante():
     for restaurante in restaurantes:
         if nome_restaurante == restaurante['nome']:
             restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
 
 
     voltar_ao_menu_principal()
